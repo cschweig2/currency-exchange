14cf87693fa4f38bded0682f8c7852f84249973b
Chelsea Becker <cschweig2@gmail.com>
2020-11-20 09:59:58 -0800

    FAIL test 1: currencyConv()

---
+++ __tests__/currencyConv.test.js
@@ -0,0 +1,8 @@
+import currencyConv from './../src/currencyConv.js';
+
+describe('function to convert currency amount', () => {
+
+  test('should conver users input to numbers', () => {
+  expect(currencyConv("50")).toEqual(0);
+  });
+});
---
+++ src/currencyConv.js
@@ -0,0 +1,5 @@
+export default function currencyConv(input, response) {
+  let dollars = parseInt(input);
+  return dollars;
+}
+
