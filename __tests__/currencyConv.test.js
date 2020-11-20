473e6fec0bb61c5b88584bd0d9167ee7ecf9045d
Chelsea Becker <cschweig2@gmail.com>
2020-11-20 10:00:32 -0800

    PASS test 1: currencyConv()

--- __tests__/currencyConv.test.js
+++ __tests__/currencyConv.test.js
@@ -3,6 +3,6 @@ import currencyConv from './../src/currencyConv.js';
 describe('function to convert currency amount', () => {
 
   test('should conver users input to numbers', () => {
-  expect(currencyConv("50")).toEqual(0);
+  expect(currencyConv("50")).toEqual(50);
   });
 });
