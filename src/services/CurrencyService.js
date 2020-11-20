e4df0a3c8edf84c2b508d726a7e28bf6fee19bf9
Chelsea Becker <cschweig2@gmail.com>
2020-11-20 09:40:40 -0800

    add functional API call

---
+++ src/services/CurrencyService.js
@@ -0,0 +1,13 @@
+export default class CurrencyService {
+  static async getCurrency(exchangeCode) {
+    try {
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      if (!response.ok) {
+        throw Error(response.status)
+      }
+      return response.json();
+    } catch(err) {
+      return Error(err);
+    }
+  }
+}
