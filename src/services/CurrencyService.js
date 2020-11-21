af7b073bcc040fdab704115caa1e9e4b8e67ec02
Chelsea Becker <cschweig2@gmail.com>
2020-11-20 16:24:46 -0800

    fix lint warnings

--- src/services/CurrencyService.js
+++ src/services/CurrencyService.js
@@ -1,9 +1,9 @@
 export default class CurrencyService {
   static async getCurrency() {
     try {
-      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
-        throw Error(response.status)
+        throw Error(response.status);
       }
       return response.json();
     } catch(err) {
